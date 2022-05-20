dfa422e5323bd0a4e96ac8c1d9012d5f4f090cef
LucJRibas <l247231@dac.unicamp.br>
2022-05-20 17:36:43 -0300

    Movimento para teste do raycasting

--- core/src/main/kotlin/trabalhofinal/screens/TestScreen.kt
+++ core/src/main/kotlin/trabalhofinal/screens/TestScreen.kt
@@ -1,14 +1,19 @@
 package trabalhofinal.screens
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Circle
+import com.badlogic.gdx.math.Rectangle
 import ktx.graphics.use
 import trabalhofinal.HEIGHT
 import trabalhofinal.MyGame
 import trabalhofinal.WIDTH
 import trabalhofinal.components.Tile
 import trabalhofinal.utils.MapReader
+import kotlin.math.abs
 import kotlin.random.Random
 
 fun randomColor(offset: Float) = Color(
@@ -20,7 +25,10 @@ fun randomColor(offset: Float) = Color(
 class TestScreen(game: MyGame): CustomScreen(game) {
 
     private val tiles = mutableListOf<MutableList<Tile>>()
-    private val text = GlyphLayout()
+//    private val text = GlyphLayout()
+
+    private val player = Circle(WIDTH/2, HEIGHT/2, 10f)
+
 
     override fun show() {
         val reader = MapReader("assets/test.map")
@@ -45,10 +53,53 @@ class TestScreen(game: MyGame): CustomScreen(game) {
     }
 
     override fun render(delta: Float) {
+//        text.setText(font, "PARTY")
+//        batch.use {
+//            font.color = randomColor(0.3f)
+//            font.draw(batch, text, WIDTH/2 - text.width/2,  HEIGHT/2 + text.height/2)
+//        }
+
+        tempController()
         renderer.use(ShapeRenderer.ShapeType.Filled){
             tiles.forEach{ line ->
                 line.forEach { tile ->
                     tile.draw(renderer)
+                }
+            }
+            renderer.color = Color.BROWN
+            renderer.circle(player.x, player.y, player.radius)
+        }
+    }
+
+    private fun tempController(){
+        val speed = 6
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) player.y += speed
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) player.y -= speed
+
+        var playerRect = Rectangle(player.x - player.radius, player.y - player.radius, 2*player.radius, 2*player.radius)
+        tiles.forEach{ line ->
+            line.forEach { tile ->
+                if (tile.color == Color.WHITE && tile.overlaps(playerRect)){
+                    if (Gdx.input.isKeyPressed(Input.Keys.S))
+                        player.y = tile.y + tile.height + player.radius
+                    else
+                        player.y = tile.y - player.radius
+                }
+            }
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) player.x -= speed
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) player.x += speed
+
+        playerRect = Rectangle(player.x - player.radius, player.y - player.radius, 2*player.radius, 2*player.radius)
+
+        tiles.forEach{ line ->
+            line.forEach { tile ->
+                if (tile.color == Color.WHITE && tile.overlaps(playerRect)){
+                    if (Gdx.input.isKeyPressed(Input.Keys.A))
+                        player.x = tile.x + tile.width + player.radius
+                    else
+                        player.x = tile.x - player.radius
                 }
             }
         }
